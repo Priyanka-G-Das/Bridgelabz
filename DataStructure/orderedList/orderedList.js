b4c11bf9ca2ec86f64dd828d766f01b245b0500e
Nikhil Osatwal <priyankadas5076@gmail.com>
2019-11-26 19:00:35 +0530

    [Priyanka] Refactor some program & pushed into remote

--- DataStructure/orderedList/orderedList.js
+++ DataStructure/orderedList/orderedList.js
@@ -2,7 +2,7 @@
 * purpose  : Read .a List of Numbers from a file and arrange it ascending Order in a Linked List.
 *  Take user input for a number, if found then pop the number out of the list else insert the number 
 * in appropriate position
-*  File     : Array2D.js
+*  File     : OrderedList.js
 *  since    : 25/11/2019
  ******************************************************************************************/
 const fs = require("fs"); //built in library for read file
