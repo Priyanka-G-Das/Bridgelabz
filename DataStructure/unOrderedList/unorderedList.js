34e7dec7021f8356b9b774fe8eaf7ce589765695
Nikhil Osatwal <priyankadas5076@gmail.com>
2019-11-26 19:13:07 +0530

    [Priyanka] Refactor some program & pushed into remote

--- DataStructure/unOrderedList/unorderedList.js
+++ DataStructure/unOrderedList/unorderedList.js
@@ -1,3 +1,11 @@
+/******************************************************************************************************
+* purpose  : Read the Text from a file, split it into words and arrange it as Linked List. 
+* Take a user input to search a Word in the List. If the Word is not found then add it to the 
+* list, and if it found then remove the word from the List. In the end save the list into a file
+*  Auther   : priyankadas
+*  File     : unorderedList.js 
+*  since    : 25/11/2019
+ */
 const fs = require("fs"); //built in library for read file
 const linkedList=require('../unOrderedList/unorderedListBL'); //import the linkedlist.js file
 const readline = require("readline-sync");
