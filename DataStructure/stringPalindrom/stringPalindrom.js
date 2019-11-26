0d05d9f6b4d8509f8dac84cf05faec33206e03d0
Nikhil Osatwal <priyankadas5076@gmail.com>
2019-11-26 19:09:45 +0530

    [Priyanka] Refactor some program & pushed into remote

--- DataStructure/stringPalindrom/stringPalindrom.js
+++ DataStructure/stringPalindrom/stringPalindrom.js
@@ -1,3 +1,9 @@
+/**************************************************************************************
+* purpose   : A palindrome is a string that reads the same forward and backward, for example, radar, toot, and madam. We would like to construct an algorithm to input a string of characters and check whether it is a palindrome.
+*  Auther   : priyankadas
+*  File     : StringPalindrom.js
+*  since    : 25/11/2019
+ ***********************************************************************************************/
 const take=require('../stringPalindrom/stringPalindromBL')
 const readline=require('readline-sync')
 const str=readline.question("enter the string");
