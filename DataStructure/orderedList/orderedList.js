617d263b8bbdf6a6a9ebd860b57a36f83722e25a
Nikhil Osatwal <priyankadas5076@gmail.com>
2019-11-26 18:54:12 +0530

    [Priyanka] Refactor some program & pushed into remote

--- DataStructure/orderedList/orderedList.js
+++ DataStructure/orderedList/orderedList.js
@@ -1,7 +1,14 @@
+/***********************************************************************************
+* purpose  : Read .a List of Numbers from a file and arrange it ascending Order in a Linked List.
+*  Take user input for a number, if found then pop the number out of the list else insert the number 
+* in appropriate position
+*  File     : Array2D.js
+*  since    : 25/11/2019
+ ******************************************************************************************/
 const fs = require("fs"); //built in library for read file
 const linkedList = require('../orderedList/orderedListBL'); //import the orderedlinkedlist.js file
 const readline = require("readline-sync");
 const data = fs.readFileSync('./NumberList.txt'); //read the data from StringList.txt file
 console.log("Numbers data read from file: \n\n" + data);
 searchNumber = readline.question("Enter a number to search"); //user input to search element
-linkedList.orderedList(data, searchNumber);     
+linkedList.orderedList(filedata, searchNumber);     
