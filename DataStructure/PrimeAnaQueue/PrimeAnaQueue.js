5ed42b8131b706934fe64da16be32388d5f5d51a
Nikhil Osatwal <priyankadas5076@gmail.com>
2019-11-26 18:57:10 +0530

    [Priyanka] Refactor some program & pushed into remote

--- DataStructure/PrimeAnaQueue/PrimeAnaQueue.js
+++ DataStructure/PrimeAnaQueue/PrimeAnaQueue.js
@@ -1,3 +1,8 @@
+/*************************************************************************************
+* purpose  : Add the Prime Numbers that are Anagram in the Range of 0 - 1000 in a Queue using the Linked List and Print the Anagrams from the Queue. Note no Collection Library can be used.
+*  File     : primeAnaQue.js
+*  since    : 25/11/2019
+ *********************************************************************************************/
 const primeAnagram = require('../PrimeAnaQueue/PrimeAnaQueueBL');
 const primeAnaQueue = require("readline-sync");
 try {
