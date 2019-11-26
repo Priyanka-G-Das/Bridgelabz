f083a2b7c72d976688ffb82b1a16e032d5408d94
Nikhil Osatwal <priyankadas5076@gmail.com>
2019-11-26 19:05:31 +0530

    [Priyanka] Refactor some program & pushed into remote

--- DataStructure/balancedParenthesis/balancedParenthesis.js
+++ DataStructure/balancedParenthesis/balancedParenthesis.js
@@ -1,11 +1,10 @@
 /***********************************************************************************
- * purpose  : Take an Arithmetic Expression such as (5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3) where parentheses are used to order 
- * the performance of operations. Ensure parentheses must appear in a balanced fashion.
-
+* purpose  : Take an Arithmetic Expression such as (5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3) where parentheses are used to order 
+* the performance of operations. Ensure parentheses must appear in a balanced fashion.
 *  Auther   : priyankadas
 *  File     : BalancedParanthesis.js
 *  since    : 25/11/2019
- */
+ ****************************************************************************************************/
 const BalPar = require('./balancedParenthesisBL')
 let input = require('readline-sync');
 let exp = input.question("Enter the expression: ")
