713ea4f4e07f6ffd41b12b4cec3785803ad1d303
Nikhil Osatwal <priyankadas5076@gmail.com>
2019-12-06 17:23:35 +0530

    [Priyanka] Refactor .some Program & pushed into remote

--- FunctionalPrograms/array2d/array2d.js
+++ FunctionalPrograms/array2d/array2d.js
@@ -1,10 +1,10 @@
-/****************************************************************************** 
+/*************************************************************************** 
 *  purpose  : a library for reading in 2D arrays of intigers,doubles and booleans from the input 
    and printing them  out to tje standard output.
 *  Auther   : priyankadas
 *  File     : Array2D.js
 *  since    : 16/11/2019
-***************************************************************************************/
+***********************************************************************************/
 const take = require('../array2d/array2dBL');
 const read = require('readline-sync');
 const row = read.question("enter the row");
