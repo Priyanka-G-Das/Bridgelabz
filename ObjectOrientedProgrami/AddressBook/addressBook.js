683c5562168e6c8840d6866b21e0b8c66912975f
Nikhil Osatwal <priyankadas5076@gmail.com>
2019-12-06 16:39:12 +0530

    [Priyanka] Refactor .some Program & pushed into remote

---
+++ ObjectOrientedProgrami/AddressBook/addressBook.js
@@ -0,0 +1,17 @@
+/******************************************************************************************************
+* Purpose : The software to be designed is a program that can be used to maintain an address book. An address book
+    holds a collection of entries, each recording a person's author,language,edition,author
+* File    :companySharesManage.js
+* Author  :priyankadas
+* Date    :1-12-2019
+ *******************************************************************************************************/
+try {
+    const fs = require('fs')
+    const address_Book = require("../AddressBook/addressBookBL")
+    let jsonFile = fs.readFileSync("/home/user/Bridgelabz/ObjectOrientedPrograming/AddressBook/addressBook.json")
+    const data = JSON.parse(jsonFile)
+    address_Book.addressOperations(data)
+    console.log(address_Book)
+} catch (e) {
+    console.log(e.message) 
+}
---
+++ ObjectOrientedProgrami/StockAccountmanagement/Test.js
@@ -0,0 +1,36 @@
+//import chai and assert package.
+const assert = require('chai').assert
+//import file
+const stock = require('../Oops/stockManagement/stockReportBlogic')
+//starts test
+describe('positive testing of total stock', function () {
+    it('total value', function () {
+        //checks the value with assert
+        assert.isNotNull(stock, 'number')
+    })
+})
+//starts test
+describe('negative testing of total stock', function () {
+    it('total value', function () {
+        //checks value with assert
+        assert.isString(stock, 'number')
+    })
+})
+//import chai and assert package.
+const assert = require('chai').assert
+//import file
+const stock = require('../Oops/stockManagement/stockReportBlogic')
+//starts test
+describe('positive testing of total stock', function () {
+    it('total value', function () {
+        //checks the value with assert
+        assert.isNotNull(stock, 'number')
+    })
+})
+//starts test
+describe('negative testing of total stock', function () {
+    it('total value', function () {
+        //checks value with assert
+        assert.isString(stock, 'number')
+    })
+})
